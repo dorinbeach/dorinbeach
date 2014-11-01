baa4d4e47b35a57afe0500e32463c79029570250
Dorin Beach <dorinbeach1@gmx.de>
2014-11-01 23:41:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dorinbeach::Application.config.secret_key_base = 'c980f7945534473c41f5b765db865496f32c4dc448af48a6aa80420ada715b5e25fb7d758c9cd2a6b2ec5bd485db15e5a090bf7dcbb82fcd056abad847579178'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dorinbeach::Application.config.session_store :cookie_store, key: '_dorinbeach_session'
